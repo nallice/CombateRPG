0880f5486172b7eb29e903fc8f124d660ce713b3
Ana Alice Dias <alicdias92@gmail.com>
2024-09-22 23:12:12 -0300

    Update Inimigos.java

--- Inimigos.java
+++ Inimigos.java
@@ -5,7 +5,7 @@ public class Inimigos {
     int dano;
     int defesa;
     int agilidade;
-    boolean duplaDefesa;
+    boolean defesaDupla;
 
     public Inimigos(String nome, int hp, int dano, int defesa, int agilidade){
         this.nome = nome;
