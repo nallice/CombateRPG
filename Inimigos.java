ef5bacb6edbb9ad12b749b9168cb604acf3b38ea
Ana Alice Dias <alicdias92@gmail.com>
2024-09-22 22:36:38 -0300

    Create Inimigos.java

---
+++ Inimigos.java
@@ -0,0 +1,17 @@
+public class Inimigos {
+
+    String nome;
+    int hp;
+    int dano;
+    int defesa;
+    int agilidade;
+    boolean duplaDefesa;
+
+    public Inimigos(String nome, int hp, int dano, int defesa, int agilidade){
+        this.nome = nome;
+        this.hp = hp;
+        this.dano = dano;
+        this.defesa = defesa;
+        this.agilidade = agilidade;
+    }
+}
