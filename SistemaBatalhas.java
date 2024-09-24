afd7d03b489199574832a2cbfc3759e059a8866b
Yasmin Dos Santos Barata <162582195+YasminSBarata@users.noreply.github.com>
2024-09-24 00:41:17 -0300

    Update SistemaBatalhas.java

--- SistemaBatalhas.java
+++ SistemaBatalhas.java
@@ -1,19 +1,18 @@
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class SistemaBatalhas {
-    private Personagem personagemB;
+    private Personagem personagem;
     private Inimigos[] inimigos;
     // private Inimigos[] inimigos2f;
     
 //Construtor da classe SistemaBatalhas
     public SistemaBatalhas(Personagem personagem){
-        this.personagemB = personagem;
+        this.personagem = personagem;
         Inimigos inimigos[] = new Inimigos[2];
-            inimigos[0]=new Inimigos("Tes1", 15, 5, 5, 5);                       
-            inimigos[1]=new Inimigos("Tes2", 15, 5, 5, 5);                       
-            inimigos[2]=new Inimigos("Tes3", 15, 5, 5, 5);                       
+            inimigos[0]=new Inimigos("Tes1", 15, 5, 5, 5,10);                       
+            inimigos[1]=new Inimigos("Tes2", 15, 5, 5, 5,10);                       
+            inimigos[2]=new Inimigos("Tes3", 15, 5, 5, 5,10);                       
 }
 
     public boolean verificacaoBatalha1(){
@@ -29,60 +28,85 @@ public class SistemaBatalhas {
         if (!vencedor) {
             return false;
         }
-        personagemB.hp = hp//novos pontos de constituição que o usuario escolher;
+        personagem.hp = hp//novos pontos de constituição que o usuario escolher;
 
     }
 
-
-
-
     private boolean batalha(Scanner scanner, Inimigos inimigo){
-        while (personagemB.getHP() > 0 && inimigo.getHp() > 0) {
-            System.out.println("Sua vida: " + personagemB.getHP());
+        while (personagem.getHP() > 0 && inimigo.getHp() > 0) {
+            System.out.println("Sua vida: " + personagem.getHP());
             System.out.println(inimigo.getNome() + " vida: " + inimigo.getHp());
 
+            turno(personagem, inimigo);
+            if (turno(personagem, inimigo)) {
             System.out.println("Escolha sua ação:");
             System.out.println("1. Atacar");
             System.out.println("2. Fugir");
-
+            System.out.println("3. usar porção");
             int choice = scanner.nextInt();
-            if (choice == 1) {
-                inimigo.Dano(player.getAttack());
-                System.out.println("Você atacou " + inimigo.getNome() + " por " + player.getAttack() + " de dano.");
+                if (choice == 1) {
+                    //turno personagem
+                    //personagem ataque
+                    System.out.println("Você atacou o inimgo com:"+ personagem.getAtaque());
+                    int danoRecebidoI = personagem.getAtaque() - inimigo.armadura;
+                    inimigo.danoRecebido(danoRecebidoI);
+                    System.out.println("Vida inimigo:"+ inimigo.getHp());
 
-                if (inimigo.getHp() > 0) {
-                    player.takeDamage(enemy.getAttack());
-                    System.out.println(inimigo.getNome() + " te atacou por " + enemy.getAttack() + " de dano.");
+                    if (inimigo.getHp()>0) {
+                        //turno inimigo
+                        Random random = new Random();
+                        int dado3Lados = random.nextInt(2)+0;
+                        if (dado3Lados==0) {
+                            //inimigo ataque
+                            System.out.println("o inimigo te atacou com"+ inimigo.ataque);
+                            double danoRecebidoP = inimigo.ataque - personagem.getDefesa();
+                            personagem.danoRecebido(danoRecebidoP);
+                            System.out.println("Sua vida:"+ personagem.getHP());
+                        }
+                        else if (dado3Lados==1) {
+                            //defender
+                            
+                            
+                        }
+                        else if (dado3Lados==2) {
+                            //usar porção
+                        }
+                    }
                 }
-            } else if (choice == 2) {
-                System.out.println("Você fugiu da batalha.");
-                return false;
+                
+
+
+
             }
+
+            
+
+        
+            // if (choice == 1) {
+
+            //     inimigo.Dano(player.getAttack());
+            //     System.out.println("Você atacou " + inimigo.getNome() + " por " + player.getAttack() + " de dano.");
+
+            //     if (inimigo.getHp() > 0) {
+            //         player.takeDamage(enemy.getAttack());
+            //         System.out.println(inimigo.getNome() + " te atacou por " + enemy.getAttack() + " de dano.");
+            //     }
+            // } else if (choice == 2) {
+            //     System.out.println("Você fugiu da batalha.");
+            //     return false;
+            // }
         }
 
-        return personagemB.getHP() > 0;
-
-
+        return personagem.getHP() > 0;
     }
 
-
-
-
-// void inimigos2f (Personagem personagemB){
-//     // this.personagemB = personagemB;
-//     this.inimigos2f = new Inimigos[]{
-//         new Inimigos("Tes1", 15, 5, 5, 5),                       
-//         new Inimigos("Tes2", 15, 5, 5, 5)                
-//     };
-// }
-
-public boolean batalha1(Personagem personMain){
-
+private boolean turno(Personagem personagem, Inimigos inimigo){
+    if (personagem.agilidade>inimigo.agilidade) {
+        return true;
+    }else{
+        return false;
+    }
 }
-
-
-
-
 
 
 
