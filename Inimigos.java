10c5f63e976e87f304b740b793343f52c386aaf4
Ana Alice Dias <alicdias92@gmail.com>
2024-09-24 18:39:08 -0300

    Update Inimigos.java

--- Inimigos.java
+++ Inimigos.java
@@ -1,4 +1,3 @@
-package JRPG;
 
 public class Inimigos {
 
@@ -8,15 +7,17 @@ public class Inimigos {
     int defesa;
     int agilidade;
     int armadura;
+    int maxHp = 10;
     // boolean defesaDupla;
 
-    public Inimigos(String nome, int hp, int ataque, int defesa, int agilidade, int armadura){
+    public Inimigos(String nome, int hp, int ataque, int defesa, int agilidade, int armadura, int maxHp){
         this.nome = nome;
         this.hp = hp;
         this.ataque = ataque;
         this.defesa = defesa;
         this.agilidade = agilidade;
         this.armadura = armadura;
+        this.maxHp = maxHp;
     }
 
     public String getNome() {
@@ -46,5 +47,12 @@ public class Inimigos {
     public void danoRecebido(int danoRecebido){
         hp -= danoRecebido;
     }
-
+    public void recuperarHP(int quantidadeCura) {
+        int cura = 7;
+        hp += cura;
+        if (hp > maxHp) {
+            hp = maxHp;  // Garante que a vida não ultrapasse o máximo
+    }
+        System.out.println(nome + " recuperou " + quantidadeCura + " de vida!");
+    }
 }
