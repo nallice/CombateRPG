a93cf65db9c327500720182009def4e3ef9e9f53
Yasmin Dos Santos Barata <162582195+YasminSBarata@users.noreply.github.com>
2024-09-24 00:43:13 -0300

    Update Armas.java

--- Armas.java
+++ Armas.java
@@ -1,3 +1,5 @@
+package JRPG;
+
 import java.util.Random;
 
 public class Armas {
@@ -5,7 +7,7 @@ public class Armas {
     //dano constante
     int danoConstante = 0;
     //dano total
-    double dano = 0;
+    int dano = 0;
     //arma leve
     int dado6Lados = random.nextInt(5) + 1;  
     int dado4Lados = random.nextInt(3) + 1;
@@ -13,14 +15,14 @@ public class Armas {
     int dado12Lados = random.nextInt(11) + 1;
     
     //dano armas leves
-    public double WarmHug(int destreza){
+    public int WarmHug(int destreza){
         danoConstante += 6;
         dano += dado6Lados + dado6Lados + dado4Lados + danoConstante;
         
         return dano;
     }
     
-    public double SunBath (int destreza){
+    public int SunBath (int destreza){
         danoConstante += 6;
         dano += dado6Lados + dado6Lados + dado4Lados + danoConstante;
         
@@ -28,18 +30,17 @@ public class Armas {
     }
     
     //armas pesadas
-    public double SunnyDay(int forca){
+    public int SunnyDay(int forca){
         danoConstante += 8;
         dano += danoConstante + dado12Lados + forca*1.5;
         
         return dano;
     }
     
-    public double MorningStar (int forca){
+    public int MorningStar (int forca){
         danoConstante += 10;
         dano += danoConstante + dado12Lados + forca*1.5;
         
         return dano;
     }
 }
-
