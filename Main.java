72b4e8e11af7b2bbfae22cf855113b038b07c943
Ana Alice Dias <alicdias92@gmail.com>
2024-09-24 23:40:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ import java.lang.Thread;
 public class Main {
     public static void main(String [] args)throws InterruptedException{
         Scanner input = new Scanner(System.in);
-        Texto texto = new Texto();
         char choice;
         
         while(true){
@@ -33,6 +32,7 @@ public class Main {
             }
         } 
     }
+    
     private static void Customizacao() throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
         
@@ -42,32 +42,54 @@ public class Main {
         System.out.print("\nDigite o nome do seu personagem: \n");
         String nome = scanner.nextLine();
         personagem.setNome(nome);
-        while (Personagem.pontos > 0) {
-            System.out.println("Distribua seus pontos de atributo. Você tem " + Personagem.pontos + " pontos para distribuir.");
+        System.out.println("Distribua seus pontos de atributo. Você tem " + personagem.pontos + " pontos para distribuir.");
+
+        int value = personagem.pontos;
+        
+        boolean verify = true;
+
+        while (verify){
+            personagem.pontos = value;
+
+            System.out.println("Pontos Restantes: " + personagem.pontos);
 
             System.out.print("Força: ");
             int forca = scanner.nextInt();
+            personagem.setForca(forca);
+            scanner.nextLine();
+                     
 
+            System.out.println("Pontos Restantes: " + personagem.pontos);
             System.out.print("Destreza: ");
             int destreza = scanner.nextInt();
-
+            personagem.setDestreza(destreza);
+            scanner.nextLine();
+            
+            System.out.println("Pontos Restantes: " + personagem.pontos);
             System.out.print("Constituição: ");
             int constituicao = scanner.nextInt();
-
+            personagem.setConstituicao(constituicao);
+            scanner.nextLine();
+            
+            System.out.println("Pontos Restantes: " + personagem.pontos);
             System.out.print("Agilidade: ");
             int agilidade = scanner.nextInt();
+            personagem.setAgilidade(agilidade);
+            scanner.nextLine();
+            
 
-            int totalPontos = forca + destreza + constituicao + agilidade;
+            System.out.println("Pontos Restantes: " + personagem.pontos);
 
-            if (totalPontos > Personagem.pontos) {
+    
+            
+            if ( personagem.pontos < 0 ) {
+                System.out.println(personagem.pontos);
                 System.out.println("Você ultrapassou o limite de pontos disponível. Tente novamente!");
-                Thread.sleep(1000);
-                Clean();  // Limpar a tela
-                TutorialCustom();
-                Start();
+
+            } else {
+                verify = false;
             }
-
-
+        }  
         
         /*while (Personagem.pontos>=0);*/
         
@@ -76,17 +98,17 @@ public class Main {
         // Escolher a arma
         Armas armas = new Armas();
         System.out.println("Escolha sua arma:");
-        System.out.println("1. Warm Hug (leve)\n2. Sun Bath (leve)\n3. Sunny Day (pesada)\n4. Morning Star (pesada)");
+        System.out.println("1. Warm Hug (leve)\n2. SunnyDay (pesada)\n");
         int escolhaArma = scanner.nextInt();
         if (escolhaArma == 1) {
             personagem.arma = armas.WarmHug(personagem.destreza);
-        } else if (escolhaArma == 2) {
+        } /*else if (escolhaArma == 2) {
             personagem.arma = armas.SunBath(personagem.destreza);
-        } else if (escolhaArma == 3) {
+        }*/ else if (escolhaArma == 2) {
             personagem.arma = armas.SunnyDay(personagem.forca);
-        } else if (escolhaArma == 4) {
+        } /*else if (escolhaArma == 4) {
             personagem.arma = armas.MorningStar(personagem.forca);
-        }
+        }*/
 
         // Escolher a armadura
         Armadura armadura = new Armadura();
@@ -105,32 +127,44 @@ public class Main {
 
         System.out.println("Seu personagem está pronto!");
         System.out.println("Nome: " + personagem.getNome());
-        System.out.println("Força: " + personagem.forca);
-        System.out.println("Destreza: " + personagem.destreza);
-        System.out.println("Constituição: " + personagem.constituicao);
-        System.out.println("Agilidade: " + personagem.agilidade);
+        System.out.println("Força: " + personagem.getForca());
+        System.out.println("Destreza: " + personagem.getDestreza());
+        System.out.println("Constituição: " + personagem.getConstituicao());
+        System.out.println("Agilidade: " + personagem.getAgilidade());
         System.out.format("HP: %d " , personagem.getHP());
         System.out.println("Arma selecionada - Dano: " + personagem.arma);
         System.out.println("Armadura selecionada - Defesa: " + personagem.armadura);
+        
+        Clean();
+       }
+
     
 
-       scanner.close();}
+    private static void Combate() {
 
+        while(true) {
+            SistemaBatalhas.verificacaoBatalha1();
+            SistemaBatalhas.verificacaoBatalha2();
+            SistemaBatalhas.verificacaoBatalha3();
+
+
+            Texto.Lose();
+
+        
+        }
     }
+
     private static void Clean(){
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
     }
-    /*private static void Lore(){
-        Clean();
-        System.out.println(" Há muito tempo, o mundo de Aurelis foi dividido entre dois povos: os Filhos do Sol e os Filhos da Lua. Suas civilizações, separadas por montanhas de fogo e mares gelados, viviam em constante guerra, lutando por crenças antigas que sustentavam a rivalidade entre eles. Os Filhos do Sol acreditavam que o Sol era a fonte da vida e poder, abençoando-os com sua energia. Já os Filhos da Lua reverenciavam a Lua como a guardiã dos mistérios e da sabedoria, sua luz prateada guiando seus passos no mundo. Ambos os povos estavam convencidos de que a outra tribo era a culpada por suas misérias e que sua destruição garantiria a supremacia. No entanto, o que nenhum deles sabia é que, no início dos tempos, eram todos uma única raça. Foram divididos por um inimigo comum, uma força sombria que operava nas sombras, manipulando-os como peças em um jogo cruel. O Vulto Eterno manipulou as mentes dos líderes da antiga civilização, plantando a semente da desconfiança entre os povos, até que as lutas fratricidas os separassem para sempre. A guerra entre os Filhos do Sol e os Filhos da Lua era, na verdade, uma farsa perpetuada por esse inimigo, que se escondia nas sombras e prosperava na divisão.");
-    }*/
+
     
     //Funções de tutoriais
     private static void TutorialCustom(){
         Clean();
-         System.out.println("Você deve customizar seu personagem! Agora explicarei como funcionara: \nEscolhera seu nome e terá 15 pontos para dividir entre seus atributos, sendo estes: força, destreza, constituiçao, agilidade,\nconstituição, sua força será importante para armas pesadas, sua destreza para armas leves, agilidade para atacar mais rápido \ne sua constituição influenciara na sua vida,voce ira jogar 3d6 para descobrir sua vida e adicionará mais a sua constituição, depois \nescolherá sua arma e armadura, e então estará pronto para o combate.");
-    }
+         System.out.println("Você deve customizar seu personagem! Agora explicarei como funcionara: \nEscolhera seu nome e terá 15 pontos para dividir entre seus atributos, sendo estes: força, destreza, constituiçao, agilidade,\nconstituição, sua força será importante para armas pesadas, sua destreza para armas leves, agilidade para atacar mais rápido \ne sua constituição influenciara na sua vida,voce ira jogar 3d6 para descobrir sua vida e adicionará mais a sua constituição, depois \nescolherá sua arma e armadura, e então estará pronto para o combate.");}
+    
     
     public static void Start() throws InterruptedException {
         Clean();
@@ -146,13 +180,15 @@ public class Main {
             switch(choice){
                     case 'P', 'p':
                         Customizacao();
+                        Combate();
                     break;
-
                     default:
                         System.out.println("Tecla inválida");
                     break;
             }
         }
-        
+    
+    
+
     }
-}
+}      
