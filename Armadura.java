31cc71ee5994bbd07011f944bd4618dc78ead051
Yasmin Dos Santos Barata <162582195+YasminSBarata@users.noreply.github.com>
2024-09-25 11:42:22 -0300

    Update Armadura.java

--- Armadura.java
+++ Armadura.java
@@ -1,31 +1,34 @@
-
-
 public class Armadura {
     int defesaConstante = 0;
     double defesa = 0;
+    public double defesaDupla;
    
-    public double Basic(int constituicao){ 
+    public double SunGoesDown(int constituicao){ 
         defesaConstante = 11;
         defesa = defesaConstante + constituicao*1.5;
         return defesa;
     }
     
-    public double Standard (int constituicao){
+    public double SunFlower (int constituicao){
         defesaConstante = 12;
         defesa = defesaConstante + constituicao*1.5;
         return defesa;
     }
-    
+    // Armaduras premiacao 2
     public double SomeWhereOverTheRainbow (int constituicao){ 
         defesaConstante = 17;
         defesa = defesaConstante + constituicao*1.5;
         return defesa;
     }
     
-    public double HereComesTheSun (int constituicao){ 
+    public double SunRoof (int constituicao){ 
         defesaConstante = 18;
         defesa = defesaConstante + constituicao*1.5;
         return defesa;
     }
-    
+   public double HereComesTheSun (int constituicao){ 
+        defesaConstante = 20;
+        defesa = defesaConstante + constituicao*1.8;
+        return defesa;
+   }   
 }
